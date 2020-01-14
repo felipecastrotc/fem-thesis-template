1b559f96e2ebd76450515a4b248db6ad638cc72e
Felipe de Castro <felipecastrotc@gmail.com>
2020-01-14 19:06:04 -0300

    Trying to change from nomenclature to glossaries

--- util.sh
+++ util.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rm -rf *.bbl *.aux *.fls *.blg *.lof *.log *.lot *.nlo *.out *.toc *.fdb_latexmk *.xml *.bcf *.bib *.nls *.ilg *.nlg *.fls *.xdv
+rm -rf *.bbl *.aux *.fls *.blg *.lof *.log *.lot *.nlo *.out *.toc *.fdb_latexmk *.xml *.bcf *.bib *.nls *.ilg *.nlg *.fls *.xdv *.glg *.glo *.gls *.glsdefs *.his-glo *.ist *.slg *.sot *.stn *.acn *.alg *.acr *.est-glo
 
 find . -name "*.aux" -type f -delete
 find . -name "__latexindent_temp.tex" -type f -delete
